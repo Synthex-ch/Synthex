06d33e0fe662a673f2300043021de391eab32ca8
pharmakatussin <80590279+pharmakatussin@users.noreply.github.com>
2026-05-10 14:32:30 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -11,4 +11,6 @@ end
 
 if table.find({92416421522960}, game.PlaceId) then -- SLIME RNG
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Synthex-ch/Synthex/refs/heads/main/Places/Slime%20RNG/init.lua"))()
+elseif table.find({89469502395769}, game.PlaceId) then -- KICK A LUCKY BLOCK
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Synthex-ch/Synthex/refs/heads/main/Places/Kick%20A%20Lucky%20Block/init.lua"))()
 end
